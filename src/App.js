eb8815612f3ce1fa83d4bcdd1eb0abf51a1c285f
Nymisha sankar <nymishasankar62@gmail.com>
2021-11-09 12:13:56 +0530

    Event bind

--- src/App.js
+++ src/App.js
@@ -6,30 +6,14 @@ import Message from './component/Message';
 import Counter from './component/Counter';
 import FunctionClick from './component/FunctionClick';
 import ClassClick from './component/ClassClick';
-
+import EventBind from './component/EventBind';
+ 
 
 function App() {
   return (
     <div className="App">
+    <EventBind/>
     
-    <ClassClick/>
-    <FunctionClick/>
-
-    <Counter/>
-
-    <Message/>
-
-    <Greet name = "Nymisha"  college = "IIT">
-      <p> This is children property</p>
-    </Greet>
-
-    <Greet name = "Shiny" college = "Oxford">
-      <button>click me</button>
-    </Greet>
-    <Greet />
-
-    <Welcome name ="Sankar" alias = "Nymisha"/>
-
     </div>
   );
 }
