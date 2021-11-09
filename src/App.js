1711be7708b117d7c3c92576543dfd3062cbf1c6
Nymisha sankar <nymishasankar62@gmail.com>
2021-11-09 12:10:53 +0530

    event

--- src/App.js
+++ src/App.js
@@ -3,29 +3,35 @@ import './App.css';
 import Greet from './component/Greet';
 import Welcome from './component/Welcome';
 import Message from './component/Message';
-import Counter  from './component/Counter';
+import Counter from './component/Counter';
+import FunctionClick from './component/FunctionClick';
+import ClassClick from './component/ClassClick';
 
- 
 
 function App() {
-    return (
-      <div className="App">
-      <Counter/>    
-      <Message/>
-  
-      <Greet name = "Nymisha"  college = "IIT">
-        <p> This is children property</p>
-      </Greet>
-  
-      <Greet name = "Honey" college = "Oxford">
-        <button>Click me</button>
-      </Greet>
-      <Greet />
-  
-      <welcome name ="Shiny" alias = "Nymisha"/>
-  
-      </div>
-    );
-  }
-  
-  export default App;
+  return (
+    <div className="App">
+    
+    <ClassClick/>
+    <FunctionClick/>
+
+    <Counter/>
+
+    <Message/>
+
+    <Greet name = "Nymisha"  college = "IIT">
+      <p> This is children property</p>
+    </Greet>
+
+    <Greet name = "Shiny" college = "Oxford">
+      <button>click me</button>
+    </Greet>
+    <Greet />
+
+    <Welcome name ="Sankar" alias = "Nymisha"/>
+
+    </div>
+  );
+}
+
+export default App;
