6b8f619a42cd7f4d6a61d2bc6ebd92d6f6b8e193
Nymisha sankar <nymishasankar62@gmail.com>
2021-11-17 14:56:09 +0530

    counters

--- src/App.js
+++ src/App.js
@@ -1,35 +1,17 @@
 import './App.css';
-import EventBind from './component/EventBind';
-import ParentComponent from './component/ParentComponent';
-import UserGreeting from './component/UserGreeting';
-import NameList from './component/NameList';
-import Table from './component/Table';
-import StyleSheet from './component/StyleSheet';
-import Inline from './component/Inline';
-import './Appstyles.css'
-import Styles from './Appstyles.module.css';
-import Stylesheet from './component/StyleSheet';
+import HookCounter2 from './components/hooks/HookCounter2';
+import HookCounter3 from './components/hooks/HookCounter3';
 
- 
+
+
+
 
 function App() {
   return (
     <div className="App">
-    <h1 className = {Styles.success}>success</h1>
-    <h1 className = 'error'>error</h1>
-
-    <Inline/>
-    <StyleSheet  enabled = {true}/>
-    <Table/>
-    <NameList/>
-    <UserGreeting/>
-
-    <ParentComponent/>
-    <EventBind/>
-    
+    <HookCounter3/>
     </div>
   );
 }
 
 export default App;
- 
